4e5f36c10074965d11fbc5fae1f39512f62122b6
ARKAM Rayane <rayane.arkam@etu.u-paris.fr>
2024-02-11 16:00:24 +0100

    Ajout de commentaires + ajout de méthodes liées à la gestion des tuiles

--- src/main/java/model/Tuiles.java
+++ src/main/java/model/Tuiles.java
@@ -11,6 +11,48 @@ public class Tuiles{
     public Tuiles() {
         this.id = ++dernierIdAttribue;
         tableauTuiles = new int[TAILLE_DU_TABLEAU];
+        // Initialiser la tuile avec des valeurs par défaut pour éviter les chemins non définis
+        for (int i = 0; i < TAILLE_DU_TABLEAU; i++) {
+            tableauTuiles[i] = -1; // -1 indique qu'aucun chemin n'est encore défini
+        }
+    }
+    /**
+     * Connecte deux points sur la tuile.
+     * 
+     * @param pointA Premier point à connecter.
+     * @param pointB Deuxième point à connecter.
+     */
+    public void connecterPoints(int pointA, int pointB) {
+        if (pointA < 0 || pointA >= TAILLE_DU_TABLEAU || pointB < 0 || pointB >= TAILLE_DU_TABLEAU) {
+            System.out.println("Indices des points invalides.");
+            return;
+        }
+        tableauTuiles[pointA] = pointB;
+        tableauTuiles[pointB] = pointA;
+    }
+    /**
+     * Effectue une rotation de la tuile de 90 degrés dans le sens des aiguilles d'une montre.
+     * Utilise la formule canonique avec ajustement pour maintenir l'orientation au Nord.
+     */
+    public void tournerTuile() {
+        int[] nouveauTableau = new int[TAILLE_DU_TABLEAU];
+        for (int i = 0; i < TAILLE_DU_TABLEAU; i++) {
+            int nouvellePosition = (i + 2) % TAILLE_DU_TABLEAU;
+            nouveauTableau[nouvellePosition] = (tableauTuiles[i] + 2) % TAILLE_DU_TABLEAU;
+        }
+        tableauTuiles = nouveauTableau;
+    }
+
+    /**
+     * Affiche les connexions actuelles de la tuile.
+     */
+    public void afficherTuile() {
+        System.out.println("Tuile ID: " + id);
+        for (int i = 0; i < TAILLE_DU_TABLEAU; i++) {
+            if (tableauTuiles[i] != -1) {
+                System.out.println("De " + i + " à " + tableauTuiles[i]);
+            }
+        }
     }
 
 }
