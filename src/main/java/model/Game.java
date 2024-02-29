65d920255f8e9882eef95982b27f270d220be268
CHERCHOUR naim <naim.cherchour@etu.u-paris.fr>
2024-02-29 21:15:38 +0100

    FrameWork sur Terminal de la classe Game.java qui représente La partie du jeu

---
+++ src/main/java/model/Game.java
@@ -0,0 +1,61 @@
+package main.java.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Game {
+    private PlateauTuiles plateau; // Le plateau de jeu
+    private List<Joueur> joueurs; // La liste des joueurs
+    // Une liste car on peut ajouter ou supprimer des joueurs
+
+    public Game(int taillePlateau, int nombreJoueurs) {
+        plateau = new PlateauTuiles(taillePlateau);
+        joueurs = new ArrayList<>();
+        for (int i = 0; i < nombreJoueurs; i++) {
+            Joueur joueur = new Joueur(0, 0, i + 1, "Joueur " + (i + 1));
+            joueurs.add(joueur);
+        }
+    }
+
+    public void jouerPartie() {
+        Scanner scanner = new Scanner(System.in);
+        TuileAdapt tuiles = new TuileAdapt();
+        plateau.afficherPlateau();
+
+        while (joueurs.size() > 1) {
+            for (Joueur joueur : joueurs) {
+                System.out.println(joueur.getPrenom() + " c'est à votre tour.");
+
+                // Demander au joueur les coordonnées de la tuile à placer
+                System.out.println("Entrez les coordonnées de la tuile à placer (ligne colonne) :");
+                int ligneTuile = scanner.nextInt();
+                int colonneTuile = scanner.nextInt();
+
+                //Une tuile est choisie aléatoirement
+                Tuile tuile = tuiles.getTuile((int) (Math.random() * 3) + 1);
+
+                // Placer la tuile sur le plateau
+                plateau.placerTuile(ligneTuile, colonneTuile, tuile, joueur);
+                plateau.actualiserPosJ(joueur);
+                plateau.afficherPlateau();
+
+                // Vérifier si le joueur a perdu
+                if (plateau.joueurPerdu(joueur)) {
+                    System.out.println(joueur.getPrenom() + " a perdu !");
+                    joueurs.remove(joueur);
+                }
+            }
+        }
+
+        System.out.println("La partie est terminée. Le gagnant est " + joueurs.get(0).getPrenom() + " !");
+    }
+
+    public static void main(String[] args) {
+        // Création d'une partie avec un plateau de taille 7 et 4 joueurs
+        Game game = new Game(7, 4);
+        // Lancement de la partie
+        game.jouerPartie();
+    }
+}
+
