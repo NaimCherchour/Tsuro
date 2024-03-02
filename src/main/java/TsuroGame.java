b365fb93ad9b4fd6979430e079db51a940f7216b
LOUNICI Sifaks <lsifaks@gmail.com>
2024-03-02 20:40:08 +0100

    Ajout d'une classe TsuroGame depuis laquelle nous pouvons lancer le jeu.

--- src/main/java/TsuroGame.java
+++ src/main/java/TsuroGame.java
@@ -1,5 +1,21 @@
 package main.java;
 
+import main.java.vue.Accueil;
+//import main.java.vue.MainMenu;
+
+import javax.swing.*;
+
 public class TsuroGame {
-    
+
+    private static Accueil accueil;
+    //private static MainMenu mainMenu;
+
+
+    // Le jeu se lance desormais depuis cette classe.
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            accueil = new Accueil();
+            accueil.show();
+        });
+    }
 }
