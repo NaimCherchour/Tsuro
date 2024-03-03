e44c03caf5cc4d3b3de390f28c13e91ad0997e66
Sami chioukh <chioukhsami7@gmail.com>
2024-03-03 10:50:31 +0100

    changement de l'attribut indexCouleur en un ordinal

--- src/main/java/model/Joueur.java
+++ src/main/java/model/Joueur.java
@@ -13,7 +13,8 @@ public class Joueur {
     private int PointEntree; // Le point d'entrée du joueur sur le plateau ( 1ère tuile ) ; est ce la position par rapport au tuile ??
     private final String prenom; // Le prénom du joueur
     private final Couleur couleur; // La couleur du joueur; c'est ce qui identifie le joueur pour la tuile
-        private static int indexCouleur = 1 ; // TODO : remplacer avec ordinal() pour obtenir l'index de la couleur
+    private static int indexCouleur = Couleur.NOIR.ordinal() + 1;; //La valeur de indexCouleur est initialisée à Couleur.NOIR.ordinal() et on fait +1 pour obtenir les couleurs suivantes
+        
 
     public enum Couleur {
         NOIR, ROUGE,BLEU,VERT,JAUNE,ORANGE,ROSE
